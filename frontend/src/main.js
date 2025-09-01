09a708fb976d4a335ca1e71c01c7e90d4da1790c
Caroline Montassier <caroline@humanisingautonomy.com>
2025-09-01 16:51:04 +0100

    Use vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify()
+
+createApp(App).use(router).use(vuetify).mount('#app')
